bd654294b9411fdea0b72d44c8d55f0682fd8afc
Nick Kenworthy <xndt98@live.com>
2021-09-23 01:50:28 -0700

    add fdn.sh

---
+++ fdn.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Filter Description Names
+# Script to quickly grab the description names from the json
+# files, so they can be added to the README
+set -euo pipefail
+
+jq '.[][][2]' "$1" | uniq | xclip -selection c
